040480eb12ef334bb1c6f084166b2e1837c32054
Abrikozs <Abrikozs@gmail.com>
2025-08-05 00:27:12 +0300

    Работа с ошибками в tasks screen

--- ToDoFIRE/Controllers/LoginViewController.swift
+++ ToDoFIRE/Controllers/LoginViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var warnLabel: UILabel!
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
 
 
 }
--- ToDoFIRE/Controllers/TasksViewController.swift
+++ ToDoFIRE/Controllers/TasksViewController.swift
@@ -9,8 +9,23 @@ import UIKit
 
 class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
         
         return cell
     }
@@ -18,6 +33,9 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
-
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
     
 }
