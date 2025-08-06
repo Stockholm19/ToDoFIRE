2c1cc8243b6d2bfcdc32b149c4e650e71547c5c4
Abrikozs <Abrikozs@gmail.com>
2025-08-06 06:14:05 +0300

    Поработал над механикой экрана с логином. Поправил скролл.

--- ToDoFIRE/Controllers/LoginViewController.swift
+++ ToDoFIRE/Controllers/LoginViewController.swift
@@ -14,17 +14,47 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var warnLabel: UILabel!
     
+    @IBOutlet weak var scrollView: UIScrollView!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+        scrollView.alwaysBounceVertical = true
+        scrollView.keyboardDismissMode = .interactive
+        
+    }
+    
+    @objc func kbDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let kbFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+
+        scrollView.contentInset.bottom = kbFrame.height
+        scrollView.verticalScrollIndicatorInsets.bottom = kbFrame.height
+    }
+
+    @objc func kbDidHide(notification: Notification) {
+        scrollView.contentInset.bottom = 0
+        scrollView.verticalScrollIndicatorInsets.bottom = 0
+    }
+    
+    func displayWarningLabel(withText text: String) {
+        warnLabel.text = text
+        warnLabel.isHidden = false
+        
+        UIView.animate(withDuration: 0.3) {
+        }
     }
     
     @IBAction func loginTapped(_ sender: UIButton) {
+        
     }
     
     @IBAction func registerTapped(_ sender: UIButton) {
+        
     }
     
 
--- ToDoFIRE/Supporting files/AppDelegate.swift
+++ ToDoFIRE/Supporting files/AppDelegate.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,9 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
